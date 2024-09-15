c00c79d236b0bc514e84d7953572bee8ceed3d8a
ironcorgi <matthew.lc.zheng@outlook.com>
2024-09-15 15:57:18 +0800

    remove old scripts

--- windows/add_environment_path_sys.ps1
+++
@@ -1,9 +0,0 @@
-if (-not ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Error "Require administrator's privilege."
-    exit 1
-}
-
-for ($i = 0; $i -lt $args.Count; $i++) {
-    $env:Path += ";$($args[$i])"
-}
-[Environment]::SetEnvironmentVariable("Path", $env:Path, [EnvironmentVariableTarget]::Machine)
--- windows/show_android_device_resolution.ps1
+++
@@ -1,8 +0,0 @@
-if ($args.Count -eq 0) {
-  adb shell wm size
-} else {
-    foreach ($dev in $args){
-        Write-Host "Specified device: $dev "
-        adb -s $dev shell wm size
-    }
-}
